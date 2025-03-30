6a92d2f0fb3006698b0ea9ba1898d8481a4ff1a5
Niels Huisman <nielsz@pm.me>
2025-03-30 13:51:14 +0200

    fuck javascript

--- shitzooi.js
+++ shitzooi.js
@@ -21,8 +21,8 @@ function planningToIcal(planning, title) {
         const isBijles = (event.backgroundColor === "lightGrey")
         
         // min een uur want het ding is dom
-        const startDate = new Date(new Date(event.start) + offset * HOUR);
-        const endDate = new Date(new Date(event.end) + offset * HOUR);
+        const startDate = new Date(new Date(event.start).getTime() + offset * HOUR);
+        const endDate = new Date(new Date(event.end).getTime() + offset * HOUR);
     
         cal.createEvent({
             summary: isBijles? event.title : t[1], // remove the name in case of huiswerkbegeleiding
